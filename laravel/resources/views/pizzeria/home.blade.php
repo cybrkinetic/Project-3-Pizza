5463a319690998955e789bc5b241e89a47f5fd44
cybrkinetic <ps252155@summacollege.nl>
2025-01-08 15:10:22 +0100

    Upload Project Part 2
    First time messed up

---
+++ laravel/resources/views/pizzeria/home.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="img/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="img/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+
+@vite('resources/css/app.css')
+</head>
+<body>
+    <header>
+        <div class="h-20">
+            <img src="img/StonksPizzaLOGO.png" class="h-30 w-60 mt-1">
+        </div> 
+        <nav>
+            <a href="" class="text-slate-500">Menu</a>
+            <a href="">Registreren</a>
+            <a href="">Inloggen</a>
+</nav>
+    </header>        
+</body>
+</html>
